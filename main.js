a301a55784ffc91ae5a613b00988fb148abb77d3
arjuns81 <95590509+arjuns81@users.noreply.github.com>
2023-05-20 11:39:26 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -4,10 +4,27 @@ function preload(){
 function setup(){
   canvas=createCanvas(600,400);
   canvas.center();
+  video=createCapture(VIDEO);
+  video.size(600,400);
+  video.hide();
+
+  posenet=ml5.poseNet(video,modelLoaded);
+  posenet.on('pose',gotPoses);
+
+  function modelLoaded(){
+    console.log('posenet');
+  }
 }
 function draw(){
-
+image(video,0,0,600,400);
 }
 function takePicture(){
-    save("myPicture.png")
+    save("myPicture.png");
+}
+function gotPoses(results){
+  if(results.length>0){
+    console.log(results);
+    console.log("nosex"+results[0].pose.nose.x);
+    console.log("nosey"+results[0].pose.nose.y);
+  }
 }
